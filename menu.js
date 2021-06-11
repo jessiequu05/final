e6c05819e33628729e89c921aeb2303e62ea1717
jessiequu05 <85744506+jessiequu05@users.noreply.github.com>
2021-06-11 22:33:11 +1000

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,8 @@
+const mainMenu = document.querySelector('.menu_btn')
+
+mainMenu.addEventListener('click',show);
+
+function show(){
+    mainMenu.style.sidplay ='flex';
+    mainMenu.style.side = '0';
+}
